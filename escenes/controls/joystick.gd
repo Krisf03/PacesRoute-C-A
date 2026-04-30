1c7c255d43487ccd70db642fbf36b8c7aaf8025a
Krisf03 <cfgarcia001@gmail.com>
2026-04-30 02:36:00 -0500

    Solo un pequeño avance en el joystick

--- escenes/controls/joystick.gd
+++ escenes/controls/joystick.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 #Variables para acceder a los nodos hijos y sus propiedades
 
+var distance:float
+var direction:Vector2
 @onready var var_range = $Range
 @onready var button = $Button
 @onready var radius = $CollisionShape2D.shape.radius
@@ -15,12 +17,20 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch: #Verifica si se presiona o se suelta el touch
 		if event.is_pressed(): #Verifica que el input del touch sea cuando se presiona
-			if global_position.distance_to(event.position) < radius: #Si el toque fue dentro de la zona del joystick
+			distance = global_position.distance_to(event.position)
+			if distance <= radius: #Si el toque fue dentro de la zona del joystick
+				direction = global_position.direction_to(button.global_position)
 				button.global_position = event.position #Tepea el centro del joystick al lugar del toque
 			
 		else: #Se ejecuta si el input es al soltar el touch
 			pass
 	if event is InputEventScreenDrag: #Verifica si se arrastra el touch
-		pass
+		distance = global_position.distance_to(event.position)
+		if distance <= radius:
+			button.global_position = event.position
+			direction = global_position.direction_to(button.global_position)
+		else:
+			direction = global_position.direction_to(event.position)
+			button.global_position = direction * radius
 		
 	
