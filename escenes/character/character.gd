9dc4789125986eb62918c7a420ee3d505c12e07a
Krisf03 <cfgarcia001@gmail.com>
2026-05-02 13:55:31 -0500

    Ahora funcionan las flechas para mover al personaje, primer commit desde la devlaptop

--- escenes/character/character.gd
+++ escenes/character/character.gd
@@ -21,6 +21,9 @@ func _physics_process(_delta: float) -> void:
 	else:
 		direction = Vector2.ZERO
 	
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	
 	velocity = direction * speed
 	move_and_slide()
 	
