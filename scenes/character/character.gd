91336aacdc2da2385f047f3a0d114c7bcaede4ad
Krisf03 <cfgarcia001@gmail.com>
2026-05-16 00:24:20 -0500

    Sistema de dialogos parte 1

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -9,6 +9,9 @@ var joystick : Area2D
 var direction : Vector2
 var speed = 3.0 * PPM * SPRITE_SCALE
 
+#Variiable para detectar si se está en dialogo
+var _is_dialogue_active := false
+
 #variable para la animación 
 @export var animated_sprite : AnimatedSprite2D
 
@@ -16,14 +19,16 @@ var speed = 3.0 * PPM * SPRITE_SCALE
 @export var interact : RayCast2D
 
 func _ready() -> void:
-	pass
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 #función para el movimiento del personaje 
 func _physics_process(_delta: float) -> void:
 	#diferencia entre dirección de joystick y dirección del teclado 
 	var joystick_direction = Vector2.ZERO
-	#calcula el input del teclado directamente en su variable
-	var keyboard_direction = Input.get_vector(
+	var keyboard_direction : Vector2
+	if _is_dialogue_active == false:
+		keyboard_direction = Input.get_vector(
 			"ui_left",
 			"ui_right",
 			"ui_up",
@@ -31,7 +36,7 @@ func _physics_process(_delta: float) -> void:
 		)
 
 	#calcula la dirección del joystick y lo mete en su variable 
-	if joystick != null and is_instance_valid(joystick):
+	if joystick != null and is_instance_valid(joystick) and _is_dialogue_active == false:
 		joystick_direction = joystick.direction
 
 	#suma las variables, para que no decidir una u otra, y se normaliza para evitar velocidades exageradas
@@ -61,3 +66,15 @@ func _animation_run():
 
 func receive_joystick(j: Area2D) -> void:
 	joystick = j
+
+#Activar dialogo
+func _on_dialogue_started(_dialogue):
+	_is_dialogue_active = true
+
+#Terminar dialogo
+func _on_dialogue_ended(_dialogue):
+	await get_tree().create_timer(0.2).timeout
+	_is_dialogue_active = false
+func _process(_delta: float) -> void:
+	if _is_dialogue_active == false:
+		pass
--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,18 +1,37 @@
 extends CharacterBody2D
 
+#Constantes de velocidad en base a la escala
 const PPM = 32
 const SPRITE_SCALE = 1
 
+#Constante que contiene la ruta del dialogo
+const TEST_GREETING = preload("uid://dejlirvax3g55")
+
+#Variables para dirección y velocidad
 var direction : Vector2
 var speed = 3.0 * PPM * SPRITE_SCALE
+#Límite de movimiento en pixeles
 var left_limit := 30
 var right_limit := 450
 
+#Variable para detectar al jugador
+var is_player_close := false
+
+#Variiable para detectar si se está en dialogo
+var _is_dialogue_active := false
+
+#Variable animación
 @export var animated_sprite : AnimatedSprite2D
 
 func _ready() -> void:
+	#Mover el personaje apenas iniciar
 	direction = Vector2(1, 0)
-	
+
+	#Detectar si está en dialogo
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
+#Movimiento del personaje de lado a lado
 func _physics_process(_delta: float) -> void:
 	if position.x < left_limit:
 		direction = Vector2(1, 0)
@@ -23,12 +42,36 @@ func _physics_process(_delta: float) -> void:
 	_calculate_flip_h()
 	_animation_run()
 
+#Voltear según a donde camina
 func _calculate_flip_h():
 	if !is_zero_approx(direction.x):
 		animated_sprite.flip_h = direction.x < 0
-		
+
+#Animación
 func _animation_run():
 	if velocity != Vector2.ZERO:
 		animated_sprite.play("walk")
 	else:
 		animated_sprite.play("idle")
+
+#Detectar al personaje para una interacción
+func _area_entered(_area):
+	is_player_close = true
+
+#Detectar que el personaje se ha alejado
+func _area_exited(_area):
+	is_player_close = false
+
+#Activar dialogo
+func _on_dialogue_started(_dialogue):
+	_is_dialogue_active = true
+
+#Terminar dialogo
+func _on_dialogue_ended(_dialogue):
+	await get_tree().create_timer(0.2).timeout
+	_is_dialogue_active = false
+
+#Estar pendiente a iniciar el dialogo
+func _process(_delta: float) -> void:
+	if is_player_close and Input.is_action_just_pressed("ui_accept") and _is_dialogue_active == false:
+		DialogueManager.show_dialogue_balloon(TEST_GREETING, "start")
